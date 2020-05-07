10bb7eff146796ce6dff0e50f402b17fae462fe3
fuwhqv <dyk777@uos.ac.kr>
2020-05-07 18:42:18 +0900

    [ISSUE-34]

--- app/src/main/java/com/example/codetour/SePosSettingContract.java
+++ app/src/main/java/com/example/codetour/SePosSettingContract.java
@@ -1,4 +1,6 @@
 package com.example.codetour;
 
 public interface SePosSettingContract {
+    interface View extends BaseContract.View{}
+    interface Presenter extends BaseContract.Presenter<View>{}
 }
--- app/src/main/java/com/example/codetour/SePosSettingPresenter.java
+++ app/src/main/java/com/example/codetour/SePosSettingPresenter.java
@@ -1,4 +1,13 @@
 package com.example.codetour;
 
-public class SePosSettingPresenter {
+public class SePosSettingPresenter implements SePosSettingContract.Presenter {
+    SePosSettingContract.View view;
+
+    @Override
+    public void setView(SePosSettingContract.View view){ this.view = view; }
+
+    @Override
+    public void releaseView(){ this.view = null; }
+
+
 }
