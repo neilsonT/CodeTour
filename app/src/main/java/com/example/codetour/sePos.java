ecc4f9c193838cc39c9b749ac2957cd50aa447e3
fuwhqv <dyk777@uos.ac.kr>
2020-05-10 15:48:15 +0900

    [ISSUE-41] 출발도착지 입력 화면 코드 변경 sePos 클래스 명세 변경: 시작점, 도착점이 String[] => List<String> 버튼 동작 추가 화면에서 버튼 하나 제거 이전 액티비티에서 받아온 정보를 토대로 화면에 띄우기 추가 화면에 입력된 정보를 저장하여 다음 액티비티에 인텐트로 전달 추가 화면의 EditText들의 ID를 동적으로 관리

--- app/src/main/java/com/example/codetour/sePos.java
+++ app/src/main/java/com/example/codetour/sePos.java
@@ -1,30 +1,37 @@
 package com.example.codetour;
 
+import java.io.Serializable;
+import java.util.List;
+
 //출발지&도착지에 대한 임시 클래스. 이후 일정에 대한 클래스로 대체 예정
-public class sePos {
+public class sePos implements Serializable {
     public int days;
-    public String[] startPos;
-    public String[] endPos;
+    public List<String> startPos;
+    public List<String> endPos;
 
     public sePos(){
         days = 4;
-        startPos = new String[this.days];
-        endPos = new String[this.days];
+        for(int i=0; i<days; ++i){
+            startPos.add("");
+            endPos.add("");
+        }
     }
 
     public sePos(int d) throws Exception{
         if(d<=0) throw new Exception();
 
         this.days = d;
-        startPos = new String[this.days];
-        endPos = new String[this.days];
+        for(int i=0; i<days; ++i){
+            startPos.add("");
+            endPos.add("");
+        }
     }
 
-    public sePos(int d, String[] st, String[] ed) throws Exception{
+    public sePos(int d, List<String> st, List<String> ed) throws Exception{
         this.days = d;
         this.startPos = st;
         this.endPos = ed;
 
-        if(d<=0 || st.length != d || ed.length != d) throw new Exception();
+        if(d<=0 || st.size() != d || ed.size() != d) throw new Exception();
     }
 }
