b586a1fe4f5eca04c7c4f387c604a4280fecff8c
fuwhqv <dyk777@uos.ac.kr>
2020-05-13 12:15:41 +0900

    [ISSUE-53] Spot, Course, TripSchedule 및 각각에 대한 Manager까지 총 6종에 대한 save/load를 위한 메소드들 추가 이들을 외부에서 호출하는 SaveLoadManager 클래스 추가

--- app/src/main/java/com/example/codetour/CourseManager.java
+++ app/src/main/java/com/example/codetour/CourseManager.java
@@ -1,5 +1,8 @@
 package com.example.codetour;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +13,37 @@ public class CourseManager implements Serializable {
     public CourseManager() {
         courseList = new ArrayList<Course>();
     }
-    public CourseManager(int difdays,int[] startTime, int[] endTime){
+    public CourseManager(int difdays, int[] startTime, int[] endTime){
         this();
        for (int i=0;i<difdays;i++){
            courseList.add(new Course(i+1,startTime,endTime));
        }
 
+    }
+
+    //생성자는 load를 위해, toJSONObj는 save를 위해 사용.
+    public CourseManager(JSONObject obj){
+        courseList = new ArrayList<Course>();
+        try {
+            JSONArray courseListTmp = obj.getJSONArray("courseList");
+            int len = courseListTmp.length();
+            for(int i=0; i<len; ++i){
+                courseList.add(new Course(courseListTmp.getJSONObject(i)));
+            }
+        }
+        catch(Exception e){}
+    }
+    public JSONObject toJSONObj(){
+        JSONObject ret = new JSONObject();
+        JSONArray courseListTemp = new JSONArray();
+        try{
+            int len = courseList.size();
+            for(int i=0; i<len; ++i){
+                courseListTemp.put(courseList.get(i).toJSONObj());
+            }
+            ret.put("courseList", courseListTemp);
+        }
+        catch(Exception e){}
+        return ret;
     }
 }
--- app/src/main/java/com/example/codetour/TripSchedule.java
+++ app/src/main/java/com/example/codetour/TripSchedule.java
@@ -1,4 +1,7 @@
 package com.example.codetour;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -63,6 +66,8 @@ public class TripSchedule implements Serializable{
 
 //>>>>>>> e0c04da052e7c5e8056f596d75eb26ae5b2e6f64
     }
+
+
     public void makeCourse(){
 
     }
@@ -76,4 +81,100 @@ public class TripSchedule implements Serializable{
     public String getName(){return name;}
     public String getStartDate(){return startDate;}
     public String getEndDate(){return endDate;}
+
+    //생성자는 load를 위해, toJSONObj는 save를 위해 사용.
+    public TripSchedule(JSONObject obj){
+        try {
+            courseManager = new CourseManager(obj.getJSONObject("courseManager"));
+            name = obj.getString("name");
+            startDate = obj.getString("startDate");
+            endDate = obj.getString("endDate");
+
+            // 대양: sd와 ed가 모두 Date형 자료형인데, 이 클래스 내에서는 이들을 다루는 메소드가 보이지 않음.
+            //      및, JSONObject로부터 Date를 바로 받아올 수 있는지 확인 필요.
+            //sd = ???
+            //ed = ???
+
+            difdays = obj.getInt("difdays");
+            pNum = obj.getInt("pNum");
+            tourBudget = obj.getInt("tourBudget");
+            accBudget = obj.getInt("accBudget");
+
+            food_selection = new ArrayList<String>();
+            JSONArray food_selection_tmp = obj.getJSONArray("food_selection");
+            int len_food = food_selection_tmp.length();
+            for(int i=0; i<len_food; ++i){
+                food_selection.add(food_selection_tmp.getString(i));
+            }
+
+            theme_selection = new ArrayList<String>();
+            JSONArray theme_selection_tmp = obj.getJSONArray("theme_selection");
+            int len_theme = theme_selection_tmp.length();
+            for(int i=0; i<len_theme; ++i){
+                theme_selection.add(theme_selection_tmp.getString(i));
+            }
+
+            startTime = new int[difdays];
+            JSONArray startTimeTmp = obj.getJSONArray("startTime");
+            endTime = new int[difdays];
+            JSONArray endTimeTmp = obj.getJSONArray("endTime");
+            startPoss = new ArrayList<String>();
+            JSONArray startPossTmp = obj.getJSONArray("startPoss");
+            endPoss = new ArrayList<String>();
+            JSONArray endPossTmp = obj.getJSONArray("endPoss");
+            for(int i=0; i<difdays; ++i){
+                startTime[i] = startTimeTmp.getInt(i);
+                endTime[i] = endTimeTmp.getInt(i);
+                startPoss.add(startPossTmp.getString(i));
+                endPoss.add(endPossTmp.getString(i));
+            }
+        }
+        catch(Exception e){}
+    }
+    public JSONObject toJSONObj(){
+        JSONObject ret = new JSONObject();
+        JSONArray food_selection_tmp = new JSONArray();
+        JSONArray theme_selection_tmp = new JSONArray();
+        JSONArray startTimeTmp = new JSONArray();
+        JSONArray endTimeTmp = new JSONArray();
+        JSONArray startPossTmp = new JSONArray();
+        JSONArray endPossTmp = new JSONArray();
+        try{
+            ret.put("courseManager", courseManager.toJSONObj());
+            ret.put("name", name);
+            ret.put("startDate", startDate);
+            ret.put("endDate", endDate);
+            //sd ??
+            //ed ??
+            ret.put("difdays", difdays);
+            ret.put("pNum", pNum);
+            ret.put("tourBudget", tourBudget);
+            ret.put("accBudget", accBudget);
+
+            int food_selection_len = food_selection.size();
+            for(int i=0; i<food_selection_len; ++i){
+                food_selection_tmp.put(food_selection.get(i));
+            }
+            ret.put("food_selection", food_selection_tmp);
+
+            int theme_selection_len = theme_selection.size();
+            for(int i=0; i<theme_selection_len; ++i){
+                theme_selection_tmp.put(theme_selection.get(i));
+            }
+            ret.put("theme_selection", theme_selection_tmp);
+
+            for(int i=0; i<difdays; ++i){
+                startTimeTmp.put(startTime[i]);
+                endTimeTmp.put(endTime[i]);
+                startPossTmp.put(startPoss.get(i));
+                endPossTmp.put(endPoss.get(i));
+            }
+            ret.put("startTime", startTimeTmp);
+            ret.put("endTime", endTimeTmp);
+            ret.put("startPoss", startPossTmp);
+            ret.put("endPoss", endPossTmp);
+        }
+        catch(Exception e){}
+        return ret;
+    }
 }
