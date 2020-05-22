063f48608dde1e26c9d3238644dd353b63c69f56
fuwhqv <dyk777@uos.ac.kr>
2020-05-22 16:17:06 +0900

    TourApiManager에 대한 설명

--- app/src/main/java/com/example/codetour/TourApiManager.java
+++ app/src/main/java/com/example/codetour/TourApiManager.java
@@ -1,5 +1,6 @@
 package com.example.codetour;
 
+//싱글톤으로 TourAPI클래스를 활용하기 위해, TourApiManager를 선언하여 모든 필드와 메소드를 static으로 함.
 public class TourApiManager {
     static TourApi tourApi = null;
     public static TourApi getInstance(){
