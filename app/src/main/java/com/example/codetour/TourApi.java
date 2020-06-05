03ad3457a2df0fb0b3c7420a40e6b3c3c38f78e9
JTO l <aoddntjs1@gmail.com>
2020-06-05 19:22:12 +0900

    좌표로 장소 받아오기
    기준점이 될 Spot 넘기면서 recommendSpot 실행하면, 기준점 Spot과 1km이내에 있는 장소 10개 받아옵니다. 대분류, 중분류는 검색시 사용하지 못해서 기준점과 같은 contentTypeId 가진 장소들만 불러오는데 수정 필요하다면 수정하겠습니다.

--- app/src/main/java/com/example/codetour/TourApi.java
+++ app/src/main/java/com/example/codetour/TourApi.java
@@ -20,7 +20,6 @@ import java.util.concurrent.ExecutionException;
 public class TourApi {
 
     private String ServiceKey="7MuLFocxJtu7I1bf8JWQrKsLl%2FAEwkKk1U0nGGDRHkCSX1%2Bxp7mB38%2FRIjMwrB%2F6x8rRBQ3Le2dUuVFHBhBbtQ%3D%3D";
-    String contentTypeId, cat1, cat2, areaCode, sigunguCode;//startDate,endDate;
 
     private final HashMap<String, Integer> areaCodeMap = new HashMap<String, Integer>(){
         {
@@ -130,100 +129,13 @@ public class TourApi {
         }};
     }
 
-    public List<Spot> getData(String contentTypeId, int areaCode, int sigunguCode, String cat1, String cat2) {
-        this.contentTypeId = contentTypeId;
-        this.areaCode = Integer.toString(areaCode);
-        this.sigunguCode = Integer.toString(sigunguCode);
-        this.cat1 = cat1;
-        this.cat2 = cat2;
-
-        GetDataAsyncTask myAsyncTask = new GetDataAsyncTask();
-        try {
-            return myAsyncTask.execute().get();
-        } catch (Exception e) {
-            return null;
-        }
-    }
-    public List<Spot> getData(String contentTypeId, int areaCode, String cat1, String cat2) {
-
-        this.contentTypeId = contentTypeId;
-        this.areaCode = Integer.toString(areaCode);
-        this.cat1 = cat1;
-        this.cat2 = cat2;
-
-        this.sigunguCode = "";
-
-        GetDataAsyncTask myAsyncTask = new GetDataAsyncTask();
-        try {
-            return myAsyncTask.execute().get();
-        } catch (Exception e) {
-            return null; //
-        }
-    }
-    public List<Spot> getData(String contentTypeId, int areaCode, int sigunguCode, String cat1) {
-        this.contentTypeId = contentTypeId;
-        this.areaCode = Integer.toString(areaCode);
-        this.sigunguCode = Integer.toString(sigunguCode);
-        this.cat1 = cat1;
-
-        this.cat2 = "";
-
-        GetDataAsyncTask myAsyncTask = new GetDataAsyncTask();
-        try {
-            return myAsyncTask.execute().get();
-        } catch (Exception e) {
-            return null; //
-        }
-    }
-    public List<Spot> getData(String contentTypeId, int areaCode, String cat1) {
-        this.contentTypeId = contentTypeId;
-        this.areaCode = Integer.toString(areaCode);
-        this.cat1 = cat1;
-
-        this.sigunguCode = "";
-        this.cat2 = "";
-        GetDataAsyncTask myAsyncTask = new GetDataAsyncTask();
-        try {
-            return myAsyncTask.execute().get();
-        } catch (Exception e) {
-            return null; //
-        }
-    }
-    public List<Spot> getData(String contentTypeId, int areaCode) {
-        this.contentTypeId = contentTypeId;
-        this.areaCode = Integer.toString(areaCode);
-
-        this.cat1 = "";
-        this.sigunguCode = "";
-        this.cat2 = "";
-
-        GetDataAsyncTask myAsyncTask = new GetDataAsyncTask();
-        try {
-            List<Spot> a=myAsyncTask.execute().get();
-            for(int i=0;i<a.size();i++)
-                System.out.println(a.get(i).getTitle());
-            return a;
-            //return (myAsyncTask.execute().get());
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
-        }
-    }
-    //클러스터링 완료 후 getData 모두 삭제 예정
-
-
     public List<Point> getPoint(int areaCode,int sigunguCode, String cat1, String cat2){
-
-        this.areaCode=Integer.toString(areaCode);
-        this.sigunguCode=Integer.toString(sigunguCode);
-        this.cat1=cat1; //대분류
-        this.cat2=cat2; //중분류
 
         List<Point> point_list = new ArrayList<>();
         GetPointAsyncTask myAsyncTask = new GetPointAsyncTask();
         try {
             //////////////////////////////////////////////////////////
-            point_list=myAsyncTask.execute(this.areaCode,this.sigunguCode,cat1,cat2).get();//전역변수 삭제할까요
+            point_list=myAsyncTask.execute(Integer.toString(areaCode),Integer.toString(sigunguCode),cat1,cat2).get();
         } catch (Exception e) {
             System.out.println(e);
         }
@@ -241,7 +153,6 @@ public class TourApi {
                 int total_num = 0;
                 int page_num = 50;
                 int num = 1;
-                //System.out.println(strings[0]+strings[1]+strings[2]+strings[3]);
                 do {
                     if(strings[1].equals("0")) {
                         urlstr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?"
@@ -385,6 +296,7 @@ public class TourApi {
                             spot.setPos(point.getX(),point.getY());
                             spot.setContentid(point.getContentid());
                             spot.setTitle(jObject.get("title"));
+                            spot.setContentTypeId(jObject.get("contenttypeid"));
 
                             if(jObject.containsKey("tel")){
                                 spot.setTel(jObject.get("tel"));
@@ -424,8 +336,93 @@ public class TourApi {
         }
     }
 
+    public List<Spot> getSpotUseDist(Spot spot) {
+
+        List<Point> points_list=new ArrayList<>();
+        List<Spot> spot_list=new ArrayList<>();
+        GetPointsUseDistAsyncTask pointAsyncTask = new GetPointsUseDistAsyncTask();
+        GetSpotAsyncTask spotAsyncTask=new GetSpotAsyncTask();
+
+        try {
+            points_list=pointAsyncTask.execute(spot).get();
+            spot_list=spotAsyncTask.execute(points_list).get();
+            return spot_list;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+
+    }
+
+    public class GetPointsUseDistAsyncTask extends AsyncTask<Spot,Void,List<Point>> {
+
+        @Override
+        protected List<Point> doInBackground(Spot... spots) {
+
+            List<Point> point_list = new ArrayList<>();
+            String urlstr, str, receiveMsg;
+            URL url = null;
+            try {
+                urlstr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/locationBasedList?"
+                        + "ServiceKey=" + ServiceKey
+                        + "&arrange=P"
+                        + "&contentTypeId="+spots[0].getContentTypeId()
+                        + "&mapX=" + spots[0].getPos()[0]
+                        + "&mapY=" + spots[0].getPos()[1]
+                        + "&radius=1000" //단위m
+                        + "&numOfRows=10" //받아오는 개수입니다.
+                        + "&MobileOS=AND&MobileApp=TestParsing&_type=json";
+
+                url = new URL(urlstr);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                if (conn.getResponseCode() == conn.HTTP_OK) {
+                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
+                    BufferedReader reader = new BufferedReader(tmp);
+                    StringBuffer buffer = new StringBuffer();
+                    while ((str = reader.readLine()) != null) {
+                        buffer.append(str);
+                    }
+                    receiveMsg = buffer.toString();
+                    reader.close();
+
+                    System.out.println(receiveMsg);
+
+                    JSONParser jsonParser = new JSONParser();
+                    JSONObject jsonObjtmp = (JSONObject) jsonParser.parse(receiveMsg);
+                    JSONObject parse_response = (JSONObject) jsonObjtmp.get("response");
+                    JSONObject parse_body = (JSONObject) parse_response.get("body");
+                    JSONObject parse_items = (JSONObject) parse_body.get("items");
+                    if (parse_body.get("items").equals("")) { //정보 없을경우
+                        System.out.println("표시할 정보가 존재하지 않습니다.");
+                        return null;
+                    }
+                    JSONArray jarray = (JSONArray) parse_items.get("item");
+
+                    for (int i = 0; i < jarray.size(); i++) {
+                        JSONObject jObject = (JSONObject) jarray.get(i);
+                        Point point = new Point();
+                        if (jObject.containsKey("mapx") && jObject.containsKey("mapy")) {
+                            point.setX(jObject.get("mapx"));
+                            point.setY(jObject.get("mapy"));
+                            point.setContentid(jObject.get("contentid"));
+                            point.setReadcount(jObject.get("readcount"));
+                            point_list.add(point);
+                        }
+                    }
+                } else {
+                    System.out.println("error");
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            return point_list;
+
+        }
+    }
 
 
+
+/*
     public List<Spot> getRestaurant(Spot spot,String classifi){ //음식점을 받아옵니다.
         GetRestaurantAsyncTask asynctask = new GetRestaurantAsyncTask();
         cat2=classifi;
@@ -437,71 +434,7 @@ public class TourApi {
             return null;
         }
     }
-    public void getExplain(Spot spot) {
-        GetExplainAsyntaskTask asynctask = new GetExplainAsyntaskTask();
-        asynctask.execute(spot);
-    }
 
-
-    protected String getUrl(int num) { //URL가져오기
-        String urlstr = "";
-        /*
-        if (contentTypeId == "15") {   //축제의 경우(임시)
-            urlstr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival?"
-                    + "ServiceKey=" + ServiceKey
-                    + "&arrange=B"   //정렬기준(조회순)
-                    //+"&areaCode=" +areaCode
-                    //+"&sigunguCode="+sigunguCode
-                    //+"&eventStartDate="+startDate
-                    //+"&eventEndDate="+endDate
-                    + "&numOfRows=5" +
-                    "&MobileOS=AND&MobileApp=TestParsing&_type=json";
-        } else { //"http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?ServiceKey=7MuLFocxJtu7I1bf8JWQrKsLl%2FAEwkKk1U0nGGDRHkCSX1%2Bxp7mB38%2FRIjMwrB%2F6x8rRBQ3Le2dUuVFHBhBbtQ%3D%3D&contentTypeId=12&numOfRows=5&MobileOS=AND&MobileApp=TestParsing&_type=json";*/
-
-        urlstr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaBasedList?"
-                + "ServiceKey=" + ServiceKey
-                + "&contentTypeId=" + contentTypeId
-                + "&areaCode=" + areaCode
-                + "&cat1=" + cat1
-                + "&cat2=" + cat2
-                + "&pageNo=" + num
-                + "&numOfRows=50"
-                + "&MobileOS=AND&MobileApp=TestParsing&_type=json";
-        return urlstr;
-    }
-    protected List<Spot> dataParse(JSONArray jarray, List<Spot> spotlist) {
-        for (int i = 0; i < jarray.size(); i++) {
-            //TourAPI 필수제공data title, contentID, contenttypeid
-            //contentID, contenttypeid 장소 세부설명 받아오는데 필요합니다.
-            Spot bus = new Spot();
-            JSONObject jObject = (JSONObject) jarray.get(i);
-
-            bus.setContentTypeId(jObject.get("contenttypeid"));
-            bus.setTitle(jObject.get("title"));
-            bus.setContentid(jObject.get("contentid"));
-
-            if (jObject.containsKey("addr1")) {
-                bus.setAddr1(jObject.get("addr1"));
-            }
-
-            //if (jObject.containsKey("firstimage")) {bus.setFirstimage(jObject.get("firstimage"));}
-
-            if (jObject.containsKey("mapx") && jObject.containsKey("mapy")) {
-                bus.setPos(jObject.get("mapx"), jObject.get("mapy"));
-            }
-            if (jObject.containsKey("tel")) {
-                bus.setTel(jObject.get("tel"));
-            }
-
-            if(jObject.containsKey("firstimage2")){
-                bus.setFirstImage2((String)jObject.get("firstimage2"));
-            }
-
-            spotlist.add(bus);
-        }
-
-        return spotlist;
-    }
     protected List<Spot> dataParse_restaurant(JSONArray jarray,List<Spot> spotlist){
 
         for (int i = 0; i < jarray.size(); i++) {
@@ -537,119 +470,6 @@ public class TourApi {
 
     }
 
-    public class GetDataAsyncTask extends AsyncTask<String, Void, List<Spot>> {
-
-        @Override
-        protected List<Spot> doInBackground(String... strings) {    //url 연결, Json Data 받아온 후 파싱.
-            List<Spot> spotList = new ArrayList<>();
-            String urlstr, str, receiveMsg;
-            URL url = null;
-
-            try {
-                for (int num = 1; num <= 1; num++) {  //한번에 5개 장소 가져옵니다. page=3
-                    urlstr = getUrl(num);
-
-                    url = new URL(urlstr);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    if (conn.getResponseCode() == conn.HTTP_OK) {
-                        InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                        BufferedReader reader = new BufferedReader(tmp);
-                        StringBuffer buffer = new StringBuffer();
-                        while ((str = reader.readLine()) != null) {
-                            buffer.append(str);
-                        }
-                        receiveMsg = buffer.toString();
-                        reader.close();
-
-                        System.out.println(receiveMsg);
-
-                        JSONParser jsonParser = new JSONParser();
-                        JSONObject jsonObjtmp = (JSONObject) jsonParser.parse(receiveMsg);
-                        JSONObject parse_response = (JSONObject) jsonObjtmp.get("response");
-                        JSONObject parse_body = (JSONObject) parse_response.get("body");
-
-                        if (parse_body.get("items").equals("")) { //정보 없을경우
-                            return null;
-                        }
-
-                        JSONObject parse_items = (JSONObject) parse_body.get("items");
-                        JSONArray jarray = (JSONArray) parse_items.get("item");
-                        spotList = dataParse(jarray, spotList);
-
-                    } else {
-                        System.out.println("error");
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-            return spotList;
-        }
-/*
-        @Override
-        protected void onPostExecute(List<Spot> result) { //doInbackground 작업이 끝난 후 실행 됨(확인용입니다)
-
-            for (int i = 0; i < result.size(); i++) {
-                System.out.println(result.get(i).getTitle());
-            }
-        }
- */
-    }
-    public class GetExplainAsyntaskTask extends AsyncTask<Spot, Void, Void> {
-        @Override
-        protected Void doInBackground(Spot... spot) {    //url 연결, Json Data 받아온 후 파싱.
-            String urlstr, str, receiveMsg;
-
-            URL url = null;
-            urlstr = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/detailCommon?"
-                    + "ServiceKey=" + ServiceKey
-                    + "&contentId=" + spot[0].getContentid()
-                    + "&overviewYN=Y"
-                    + "&MobileOS=AND&MobileApp=TestParsing&_type=json";
-
-            try {
-                url = new URL(urlstr);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                if (conn.getResponseCode() == conn.HTTP_OK) {
-                    InputStreamReader tmp = new InputStreamReader(conn.getInputStream(), "UTF-8");
-                    BufferedReader reader = new BufferedReader(tmp);
-                    StringBuffer buffer = new StringBuffer();
-                    while ((str = reader.readLine()) != null) {
-                        buffer.append(str);
-                    }
-                    receiveMsg = buffer.toString();
-                    reader.close();
-
-                    System.out.println(receiveMsg);
-
-                    JSONParser jsonParser = new JSONParser();
-                    JSONObject jsonObjtmp = (JSONObject) jsonParser.parse(receiveMsg);
-                    JSONObject parse_response = (JSONObject) jsonObjtmp.get("response");
-                    JSONObject parse_body = (JSONObject) parse_response.get("body");
-
-                    if (parse_body.get("items").equals("")) { //정보 없을경우
-                        return null;
-                    }
-
-                    JSONObject parse_items = (JSONObject) parse_body.get("items");
-                    JSONObject parse_item = (JSONObject) parse_items.get("item");
-
-                    if (parse_item.containsKey("overview")) {
-                        spot[0].setExplain(parse_item.get("overview"));
-                        System.out.println("in!");
-                    }
-
-                } else {
-                    System.out.println("error");
-                }
-
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.out.println("error111");
-            }
-            return null;
-        }
-    }
     public class GetRestaurantAsyncTask extends AsyncTask<Spot, Void, List<Spot>> {    //SpotList를 가져올까요?
         @Override
         protected List<Spot> doInBackground(Spot... spot) {    //url 연결, Json Data 받아온 후 파싱.
@@ -707,4 +527,6 @@ public class TourApi {
             return tmp_list;
         }
     }
+
+ */
 }
